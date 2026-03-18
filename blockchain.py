8ae1f30fbac87f1149b1b23d9ec5534bae028635
Guilherme Lima <ghduep@outlook.com>
2026-03-18 10:58:27 -0300

    implementing utxo

--- blockchain.py
+++ blockchain.py
@@ -30,6 +30,7 @@ class Blockchain:
         self.chain = []
         self.pending_transactions = []
         self.create_genesis_block()
+        self.utxos = {}
         self.difficulty = 4
         self.mining_reward = 50
 
@@ -45,88 +46,127 @@ class Blockchain:
             print("Invalid block. Previous hash does not match.")
     
     def proof_of_work(self, block):
-        block.nonce = 0
-        computed_hash = block.compute_hash()
-        while not computed_hash.startswith('0' * self.difficulty):
+        while True:
+            hash_attempt = block.compute_hash()
+            if hash_attempt.startswith("0" * self.difficulty):
+                return hash_attempt
             block.nonce += 1
-            computed_hash = block.compute_hash()
-
-        return computed_hash
     
-    def add_transaction(self, transaction):
-        if transaction.is_valid():
-            self.pending_transactions.append(transaction)
+    def is_valid_transaction(self, tx):
+        if len(tx.inputs) == 0:
+            return True
+
+        input_total = 0
+        output_total = 0
+
+        for i, tx_input in enumerate(tx.inputs):
+            key = (tx_input.tx_id, tx_input.output_index)
+
+            if key not in self.utxos:
+                return False
+            
+            referenced_output = self.utxos[key]
+
+            if referenced_output.recipient != tx_input.public_key:
+                return False
+            
+            try:
+                vk = ecdsa.VerifyingKey.from_string(
+                    bytes.fromhex(tx_input.public_key),
+                    curve=ecdsa.SECP256k1
+                )
+
+                message = tx.compute_hash().encode()
+
+                if not vk.verify(bytes.fromhex(tx_input.signature), message):
+                    return False
+            except:
+                return False
+            
+            input_total += referenced_output.amount
+
+            for output in tx.outputs:
+                output_total += output.amount
+
+            return input_total >= output_total
+    
+    def add_transaction(self, tx):
+        if self.is_valid_transaction(tx):
+            self.pending_transactions.append(tx)
         else:
             print("Invalid transaction!")
     
-    def mine_pending_transactions(self, miner_address):
-        reward_tx = Transaction("MINING_REWARD", miner_address, self.mining_reward)
+    def update_utxos(self, block):
+        for tx in block.transactions:
+            tx_id = tx.compute_hash()
+
+            for tx_input in tx.inputs:
+                key = (tx_input.tx_id, tx_input.output_index)
+                if key in self.utxos:
+                    del self.utxos[key]
+            
+            for index, output in enumerate(tx.outputs):
+                self.utxos[(tx_id, index)] = output
+
+    def mine(self, miner_address):
+        reward_tx = Transaction([], [TxOutput(miner_address, self.mining_reward)])
         self.pending_transactions.append(reward_tx)
 
-        new_block = Block(len(self.chain), [tx.__dict__ for tx in self.pending_transactions], self.chain[-1].hash)
-        new_block.hash = self.proof_of_work(new_block)
-        self.chain.append(new_block)
+        block = Block(len(self.chain), self.pending_transactions, self.chain[-1].hash)
+        block.hash = self.proof_of_work(block)
+
+        self.chain.append(block)
+        self.update_utxos(block)
 
         self.pending_transactions = []
 
-    def get_balance(self, public_key):
+    def get_balance(self, address):
         balance = 0
-        for block in self.chain:
-            for tx in block.transactions:
-                if tx["sender"] == public_key:
-                    balance -= tx["amount"]
-                if tx["recipient"] == public_key:
-                    balance += tx["amount"]
+        for (tx_id, index), output in self.utxos.items():
+            if output.recipient == address:
+                balance += output.amount
         return balance
 
-class Transaction:
-    def __init__(self, sender, recipient, amount, signature=None):
-        self.sender = sender
+class TxOutput:
+    def __init__(self, recipient, amount):
         self.recipient = recipient
         self.amount = amount
+
+class TxInput:
+    def __init__(self, tx_id, output_index, signature=None, public_key=None):
+        self.tx_id = tx_id
+        self.output_index = output_index
         self.signature = signature
+        self.public_key = public_key
+
+class Transaction:
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.timestamp = time.time()
+        self.signature = None
 
     def compute_hash(self):
-        tx_dict = {
-            "sender": self.sender,
-            "recipient": self.recipient,
-            "amount": self.amount
-        }
-        tx_string = json.dumps(tx_dict, sort_keys=True)
-        return hashlib.sha256(tx_string.encode()).digest()
+        data = json.dumps(self.__dict__, default=lambda o: o.__dict__, sort_keys=True)
+        return hashlib.sha256(data.encode()).hexdigest()
     
-    def sign_transaction(self, private_key):
-        sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key), curve=ecdsa.SECP256k1)
-        signature_bytes = sk.sign(self.compute_hash())
-        self.signature = signature_bytes.hex()
-    
-    def is_valid(self):
-        if self.sender == "MINING_REWARD":
-            return True
-        if not self.signature:
-            return False
-        vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(self.sender), curve=ecdsa.SECP256k1)
-        try:
-            return vk.verify(
-                bytes.fromhex(self.signature),
-                self.compute_hash()
-            )
-        except ecdsa.BadSignatureError:
-            return False
+    def sign_transaction(self, private_key_hex):
+        sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key_hex), curve=ecdsa.SECP256k1)
+        self.signature = sk.sign(self.compute_hash().encode()).hex()
+
+    def sign_input(self, input_index, private_key_hex):
+        tx_input = self.inputs[input_index]
+
+        sk = ecdsa.SigningKey.from_string(
+            bytes.fromhex(private_key_hex),
+            curve=ecdsa.SECP256k1
+        )
+
+        message = self.compute_hash().encode()
+        signature = sk.sign(message)
+
+        tx_input.signature = signature.hex()
+        tx_input.public_key = binascii.hexlify(
+            sk.get_verifying_key().to_string() #type: ignore
+        ).decode()
         
-if __name__ == "__main__":
-    priv1, pub1 = generate_keys()
-    priv2, pub2 = generate_keys()
-    print("Wallet 1: ", pub1)
-    print("Wallet 2: ", pub2)
-
-    bc = Blockchain()
-
-    tx1 = Transaction(pub1, pub2, 20)
-    tx1.sign_transaction(priv1)
-    bc.add_transaction(tx1)
-
-    bc.mine_pending_transactions(pub1)
-
-    print("Wallet 1 balance: ", bc.get_balance(pub1))
-    print("Wallet 2 balance: ", bc.get_balance(pub2))
